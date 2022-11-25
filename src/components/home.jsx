4c5ae6073e8be15d45d5523080c2810a6e581464
Safar Abbase <safarabbas73.sa@gmail.comm>
2022-11-25 08:35:32 +0300

    home marquee change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -16,11 +16,7 @@ function Home() {
         <div className="header-1">
           <div className="form-container">
             <div className="marqueeHome">
-              <p
-                direction="left"
-                scrollamount="8"
-                style={{ textTransform: "none" }}
-              >
+              <p style={{ textTransform: "none" }}>
                 Hijab-e-Fatimi's aim is to spread sunna of Syeda Fatima Zahra
                 s.A. She says to her belovers that women's favourite ornament is
                 abaya through which she can live in janna forever.
